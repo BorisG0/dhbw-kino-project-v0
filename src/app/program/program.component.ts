21e4a8d7a4318e56876f5ca2ae5fe7b181df2ecd
Boris Gratchev <boris@gratchev.com>
2022-09-06 21:30:42 +0200

    added list of movies

--- src/app/program/program.component.ts
+++ src/app/program/program.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MovieService } from '../movie.service';
+
+import { Movie } from '../movie';
+
+
 @Component({
   selector: 'app-program',
   templateUrl: './program.component.html',
   styleUrls: ['./program.component.css']
 })
 export class ProgramComponent implements OnInit {
+  movies: Movie[] = [];
 
-  constructor() { }
+  constructor(private movieService: MovieService) { }
 
   ngOnInit(): void {
+    this.getMovies();
+  }
+
+  getMovies(): void{
+    this.movieService.getMovies().subscribe(movies => this.movies = movies);
   }
 
 }
