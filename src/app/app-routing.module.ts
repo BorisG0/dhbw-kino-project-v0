d52e6d9967af8d1d705d1c9c7a7eaf266ed14487
Boris Gratchev <boris@gratchev.com>
2022-09-07 15:59:32 +0200

    added dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ProgramComponent } from './program/program.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: 'program', component: ProgramComponent}
+  { path: 'program', component: ProgramComponent},
+  { path: 'dashboard', component: DashboardComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProgramComponent } from './program/program.component';
 import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProgramComponent
+    ProgramComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
