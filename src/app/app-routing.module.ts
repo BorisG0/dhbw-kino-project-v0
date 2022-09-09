c9f40c9fa33656e1bd45484ecbab955687a0a90d
Boris Gratchev <boris@gratchev.com>
2022-09-09 11:23:42 +0200

    added movie detail component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ProgramComponent } from './program/program.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 
 const routes: Routes = [
   { path: 'program', component: ProgramComponent},
-  { path: 'dashboard', component: DashboardComponent}
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'detail/:id', component: MovieDetailComponent},
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full'}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { ProgramComponent } from './program/program.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProgramComponent,
-    DashboardComponent
+    DashboardComponent,
+    MovieDetailComponent
   ],
   imports: [
     BrowserModule,
