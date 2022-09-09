6414c3f47af705764a2259cd46be7a994e03dff3
Boris Gratchev <boris@gratchev.com>
2022-09-09 11:54:25 +0200

    working message system

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 
 import { Observable, of } from 'rxjs';
 
+import { MessageService } from './message.service';
+
 import { Movie } from './movie';
 import { MOVIES } from './mock-movies';
 
@@ -10,15 +12,17 @@ import { MOVIES } from './mock-movies';
 })
 export class MovieService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getMovies(): Observable<Movie[]> {
     const movies = of(MOVIES);
+    this.messageService.add('MovieService: fetched Movies');
     return movies;
   }
 
   getMovie(id: number): Observable<Movie> {
     const movie = MOVIES.find(m => m.id === id)!;
+    this.messageService.add('MovieService: fetched Movie')
     return of(movie);
   }
 }
