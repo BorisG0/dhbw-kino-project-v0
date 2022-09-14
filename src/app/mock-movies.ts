0c28bb20fdcd4c10ccd1cf7e27dfa6832b5f51e2
schollmeier <martin@schollmeier.eu>
2022-09-14 16:23:47 +0200

    Test Commit

--- src/app/mock-movies.ts
+++ src/app/mock-movies.ts
@@ -4,5 +4,6 @@ export const MOVIES: Movie[] = [
     { id: 0, name: 'Minionsss', studio: 'Universal', length: 110},
     { id: 1, name: 'Joker', studio: 'Warner Bros.', length: 200},
     { id: 2, name: 'American Psycho', studio: 'Lionsgate', length: 150},
-    { id: 3, name: 'Matrix', studio: 'Warner Bros.', length: 210}
+    { id: 3, name: 'Matrix', studio: 'Warner Bros.', length: 210},
+    { id: 3, name: 'Matrix Ressurrections', studio: 'Warner Bros.', length: 210}
 ];
