d2b76c217fc221da79ab6b56098d649421e0579b
LucaGrab <luca@grabowski.com>
2022-09-14 15:58:21 +0200

    test commit luca

--- src/app/mock-movies.ts
+++ src/app/mock-movies.ts
@@ -1,7 +1,7 @@
 import { Movie } from "./movie";
 
 export const MOVIES: Movie[] = [
-    { id: 0, name: 'Minions', studio: 'Universal', length: 110},
+    { id: 0, name: 'Minionsss', studio: 'Universal', length: 110},
     { id: 1, name: 'Joker', studio: 'Warner Bros.', length: 200},
     { id: 2, name: 'American Psycho', studio: 'Lionsgate', length: 150},
     { id: 3, name: 'Matrix', studio: 'Warner Bros.', length: 210}
