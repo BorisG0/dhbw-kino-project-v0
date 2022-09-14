105c3b6794d80aa22ae726e334d81e5134f4c309
Boris Gratchev <boris@gratchev.com>
2022-09-14 16:58:24 +0200

    fixed id

--- src/app/mock-movies.ts
+++ src/app/mock-movies.ts
@@ -5,5 +5,5 @@ export const MOVIES: Movie[] = [
     { id: 1, name: 'Joker', studio: 'Warner Bros.', length: 200},
     { id: 2, name: 'American Psycho', studio: 'Lionsgate', length: 150},
     { id: 3, name: 'Matrix', studio: 'Warner Bros.', length: 210},
-    { id: 3, name: 'Matrix Ressurrections', studio: 'Warner Bros.', length: 210}
+    { id: 4, name: 'Matrix Ressurrections', studio: 'Warner Bros.', length: 210}
 ];
