2212fddea897f378c0e3fb6cee1ef1b7da66817c
Boris Gratchev <boris@gratchev.com>
2022-09-17 12:35:47 +0200

    changed seat mock data

--- src/app/mock-seats.ts
+++ src/app/mock-seats.ts
@@ -2,9 +2,9 @@ import { Seat } from "./seat";
 
 export const SEATS: Seat[] = [
     {row: "a", numberInRow: 1, isOccupied: false},
-    {row: "a", numberInRow: 2, isOccupied: false},
-    {row: "a", numberInRow: 3, isOccupied: false},
-    {row: "a", numberInRow: 4, isOccupied: false},
+    {row: "a", numberInRow: 2, isOccupied: true},
+    {row: "a", numberInRow: 3, isOccupied: true},
+    {row: "a", numberInRow: 4, isOccupied: true},
     {row: "b", numberInRow: 1, isOccupied: false},
     {row: "b", numberInRow: 2, isOccupied: false},
     {row: "b", numberInRow: 3, isOccupied: false},
