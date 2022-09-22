fc7d0c5762f30bf24dc7fa4cc4c07871734e91a4
LucaGrab <luca@grabowski.com>
2022-09-22 20:12:36 +0200

    edited movie detail page and enhanced movie ts

--- src/app/mock-movies.ts
+++ src/app/mock-movies.ts
@@ -1,9 +1,9 @@
 import { Movie } from "./movie";
 
 export const MOVIES: Movie[] = [
-    { id: 0, name: 'Minionsss', studio: 'Universal', length: 110},
-    { id: 1, name: 'Joker', studio: 'Warner Bros.', length: 200},
-    { id: 2, name: 'American Psycho', studio: 'Lionsgate', length: 102},
-    { id: 3, name: 'Matrix', studio: 'Warner Bros.', length: 210},
-    { id: 4, name: 'Matrix Ressurrections', studio: 'Warner Bros.', length: 210}
+    { id: 0, name: 'Minionsss', studio: 'Universal', length: 110, cast: 'Platzhalter für den Cast', discription: 'Das ist ein sehr spannender Film, denn in diesem Film geht es um die Nimions. Das ist ein sehr spannender Film, denn in diesem Film geht es um die Nimions. '},
+    { id: 1, name: 'Joker', studio: 'Warner Bros.', length: 200, cast: 'Platzhalter für den Cast', discription: 'Das ist ein sehr spannender Joker Film'},
+    { id: 2, name: 'American Psycho', studio: 'Lionsgate', length: 102, cast: 'Platzhalter für den Cast', discription: 'Der Film über amis ist auch spannend Der Film über amis ist auch spannend Der Film über amis ist auch spannend Der Film über amis ist auch spannend Der Film über amis ist auch spannend Der Film über amis ist auch spannend'},
+    { id: 3, name: 'Matrix', studio: 'Warner Bros.', length: 210, cast: 'Platzhalter für den Cast', discription: 'Matrix ist auch sehr spannend und die beschreibung nicht sehr lange.'},
+    { id: 4, name: 'Matrix Ressurrections', studio: 'Warner Bros.', length: 210, cast: 'Platzhalter für den Cast', discription: 'Die Beschreibung für den zweiten Matrix Film ist wirklich sehr lang. Die Beschreibung für den zweiten Matrix Film ist wirklich sehr lang. Die Beschreibung für den zweiten Matrix Film ist wirklich sehr lang. Die Beschreibung für den zweiten Matrix Film ist wirklich sehr lang. Die Beschreibung für den zweiten Matrix Film ist wirklich sehr lang. Die Beschreibung für den zweiten Matrix Film ist wirklich sehr lang.'}
 ];
--- src/app/movie.ts
+++ src/app/movie.ts
@@ -3,4 +3,6 @@ export interface Movie {
     name: string;
     studio: string;
     length: number;  //length in minutes
+    discription: string;
+    cast: string;
 }
