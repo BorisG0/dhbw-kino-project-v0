6fee2e27f91b103e34d280bf0275d876542c297b
Boris Gratchev <boris@gratchev.com>
2022-10-03 15:35:48 +0200

    movie class matching backend

--- src/app/mock-movies.ts
+++ src/app/mock-movies.ts
@@ -7,7 +7,7 @@ export const MOVIES: Movie[] = [
     // { id: 3, title: 'Matrix', studio: 'Warner Bros.', duration: 210, cast: 'Platzhalter für den Cast', discription: 'Matrix ist auch sehr spannend und die beschreibung nicht sehr lange.'},
     // { id: 4, title: 'Matrix Ressurrections', studio: 'Warner Bros.', duration: 210, cast: 'Platzhalter für den Cast', discription: 'Die Beschreibung für den zweiten Matrix Film ist wirklich sehr lang. Die Beschreibung für den zweiten Matrix Film ist wirklich sehr lang. Die Beschreibung für den zweiten Matrix Film ist wirklich sehr lang. Die Beschreibung für den zweiten Matrix Film ist wirklich sehr lang. Die Beschreibung für den zweiten Matrix Film ist wirklich sehr lang. Die Beschreibung für den zweiten Matrix Film ist wirklich sehr lang.'}
 
-    {id: 0, title: 'Matrix', duration: 200, ageRestriction: 16, imageName: 'img0'},
-    {id: 1, title: 'American Psycho', duration: 102, ageRestriction: 18, imageName: 'img1'},
-    {id: 2, title: 'Minions', duration: 120, ageRestriction: 0, imageName: 'img2'}
+    // {id: 0, title: 'Matrix', duration: 200, ageRestriction: 16, imageName: 'img0'},
+    // {id: 1, title: 'American Psycho', duration: 102, ageRestriction: 18, imageName: 'img1'},
+    // {id: 2, title: 'Minions', duration: 120, ageRestriction: 0, imageName: 'img2'}
 ];
