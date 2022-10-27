92adb9bfd310bd57c0438056fb1be76dd5d39a8d
LucaGrab <luca@grabowski.com>
2022-10-27 14:28:22 +0200

    adjustments on registartion and login

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -3,6 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import { AppComponent } from '../app.component';
 import { Customer } from '../customer';
 import { MovieService } from '../movie.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+
 
 
 
@@ -19,8 +22,13 @@ export class RegistrationComponent implements OnInit {
   enteredName: string = "";
   enteredEmail: string = "";
   enteredPassword: string = "";
+  acceptedAGB: boolean = false;
 
-  constructor(    private movieService: MovieService
+  constructor(    private movieService: MovieService,
+    private router: Router,
+    private _snackBar: MatSnackBar,
+
+
 
     ) { }
 
@@ -31,20 +39,22 @@ export class RegistrationComponent implements OnInit {
   }
 
   onPressAddUser() {
-    console.log(this.enteredFirstName + this.selectedDate + this.enteredName + this.enteredEmail + this.enteredPassword)
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        this.movieService.addCustomer({mailAdress: this.enteredEmail, lastName : this.enteredName, firstName: this.enteredFirstName, birthDate: this.selectedDate, postalcode: 67654, houseNumber: 4, location: "Osthofen", street: "myStreet", countryCode: "DE", mobileNumber: "0234324", password: this.enteredPassword}).subscribe(
-        data => {
-          console.log(data)
-
-          resolve(0);
-        }
-        )
-
-
-      }, 0)
-    })
+    if(this.enteredEmail != "" && this.enteredFirstName != "" && this.enteredName != "" && this.enteredPassword != "" && this.selectedDate != new Date() && this.selectedDate != null && this.acceptedAGB == true){
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          this.movieService.addCustomer({mailAdress: this.enteredEmail, lastName : this.enteredName, firstName: this.enteredFirstName, birthDate: this.selectedDate, postalcode: 67654, houseNumber: 4, location: "Osthofen", street: "myStreet", countryCode: "DE", mobileNumber: "0234324", password: this.enteredPassword}).subscribe(
+          data => {
+            console.log(data)
+            resolve(0);
+          }
+          )
+        }, 0)
+      })
+    }
+    else{
+      this._snackBar.open("Daten fehlerhaft", "Okay")
+    }
+  return;
     let newCustomer: Customer;
   }
 
